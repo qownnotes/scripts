d014d79ef10adf6f5215e855cd8afe773e3fb208
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-17 17:10:03 +0200

    pbek/QOwnNotes#2772 add: minify json result

--- .github/workflows/scripts/generate-index.sh
+++ .github/workflows/scripts/generate-index.sh
@@ -3,9 +3,10 @@
 # Generate index of all script metadata
 #
 
-FILE_NAME=index.json
+TMP_FILE_NAME=/tmp/temp.json
+RESULT_FILE_NAME=index.json
 
-echo "[" >> ${FILE_NAME}
+echo "[" > ${TMP_FILE_NAME}
 
 # Search for all files named "info.json" and store their paths in an array
 files=( $(find . -name "info.json") )
@@ -13,13 +14,16 @@ files=( $(find . -name "info.json") )
 # Loop through the array of files and concatenate their contents into a new file
 for (( i=0; i<${#files[@]}; i++ ))
 do
-  cat "${files[i]}" >> ${FILE_NAME}
+  cat "${files[i]}" >> ${TMP_FILE_NAME}
   if [ $i -ne $((${#files[@]}-1)) ]
   then
-    echo "," >> ${FILE_NAME}
+    echo "," >> ${TMP_FILE_NAME}
   fi
 done
 
-echo "]" >> ${FILE_NAME}
+echo "]" >> ${TMP_FILE_NAME}
 
-echo "Done! Concatenated info.json files can be found in ${FILE_NAME}."
+# Minify json
+jq -r tostring < ${TMP_FILE_NAME} > ${RESULT_FILE_NAME}
+
+echo "Done! Concatenated info.json files can be found in ${RESULT_FILE_NAME}."
